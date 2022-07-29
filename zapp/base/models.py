76c4c7d210c31643c36976d49c4cec5ec1baebd2
Lioncat2002 <sankhayan2002@gmail.com>
2022-07-29 23:01:14 +0530

    feat: added models for music and album

--- zapp/base/models.py
+++ zapp/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Album(models.Model):
+    artist=models.CharField(max_length=200)
+    album_title=models.CharField(max_length=200)
+    genre=models.CharField(max_length=200)
+
 # Create your models here.
+class Music(models.Model):
+    album=models.ForeignKey(Album,on_delete=models.CASCADE)
+    song=models.FileField(upload_to="songs/")
